a4f667d790ee34d9cb770b3a0039c3ceb2589cb1
MooCat <43627985+mooleshacat@users.noreply.github.com>
2022-05-30 02:00:45 -0400

    changed directory structure

---
+++ bin/miner-boot.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#
+# miner-boot.sh
+#
+# Description:
+#   miner-boot is a script that will boot your miner for you.
+#   miner-boot is programmed to work with xmrig, lolMiner, and wildrig-multi for now.
+#   you may post issue ticket on github to request additonal miners.
+#   each miner is different and must be added in manually.
+#
+# Startup options:
+#
+# start <cpu|gpu|all> - start the miner(s)
+# stop <cpu|gpu|all> - stop the miner(s)
+# log <cpu|gpu> - watch logs for 'gpu' or 'cpu'
+# cron <install|remove> <cpu|gpu|all> - install/remove cronjob for cpu/gpu/all
+#
+# miners update - download and recompile miners
+#
+
+# First things first, let's source the config file
+source ../etc/config
+
+# Get argument 1 into variable
+# Get argument 2 into variable
+
+# Now let's process argument 1
+  # Now let's process argument 2
+  
+ 
