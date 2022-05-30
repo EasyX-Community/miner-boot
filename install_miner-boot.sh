5bce56773ac414316d5f6229b37f7475ffbacaa1
MooCat <43627985+mooleshacat@users.noreply.github.com>
2022-05-30 02:00:31 -0400

    changed directory structure

--- install_miner-boot.sh
+++ install_miner-boot.sh
@@ -7,5 +7,21 @@
 #   Install and configure miner-boot scripts
 #
 
+# First things first, let's source the config file
+source etc/config
 
- 
+
+# Ask the user which miners to enable
+
+
+# Download and compile the miner(s)
+
+
+# Add ${PWD}/bin to PATH
+
+
+# Ask the user about auto-update cronjob
+
+
+# Rewrite config file
+
